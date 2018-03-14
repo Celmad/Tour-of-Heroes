d92c03e2077d44756fe10e083ac8af9d3b1683b0
Celmad <manuelalaminosd@gmail.com>
2018-03-14 20:13:16 +0000

    Added Router to navigate among different components.

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,12 +1,11 @@
 import { Injectable } from '@angular/core';
 
-import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
-
-import { MessageService } from './message.service';
-
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
+
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+import { MessageService } from './message.service';
 
 @Injectable()
 export class HeroService {
@@ -19,5 +18,9 @@ export class HeroService {
     return of(HEROES);
   }
 
-
+  getHero(id: number): Observable<Hero> {
+    // Todo: send the message _after_ fetching the hero
+    this.messageService.add(`HeroService: fetched hero id=${id}`); // backticks (``) define a JavaScript template literal for embedding the id
+    return of(HEROES.find(hero => hero.id === id));
+  }
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Hero} from '../hero';
-import { HeroService } from '../hero.service'
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,23 +9,16 @@ import { HeroService } from '../hero.service'
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  selectedHero: Hero;
-
   heroes: Hero[];
 
   constructor(private heroService: HeroService) { }
-  
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
+
+  ngOnInit() {
+    this.getHeroes();
   }
 
   getHeroes(): void {
     this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes);
   }
-
-  ngOnInit() {
-    this.getHeroes();
-  }
-
 }
